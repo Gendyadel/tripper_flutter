b8ac50cd57c5f3074aca0036d812246408ecb110
Gendyadel <Gendyadel@users.noreply.github.com>
2021-05-29 02:30:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:tripper_flutter/src/app_root.dart';
+
+void main() {
+  runApp(AppRoot());
+}
+
---
+++ lib/src/app_root.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppRoot extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tripper',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: ,
+    );
+  }
+}
